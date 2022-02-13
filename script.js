5df01ed3bc1c4fcad5f8864cfa40962c31d819a0
Zeyad Tarek <Zeyad.ta01@gmail.com>
2022-02-13 16:25:34 +0200

    Holding button

--- script.js
+++ script.js
@@ -18,33 +18,22 @@ player2ScoreEl.textContent = 0;
 diceEl.classList.add("hidden");
 
 btnRoll.addEventListener("click", rollingDice);
+btnHold.addEventListener("click", holdScore);
 
-// the total scores
-const scores = [0, 0];
+let playing = true;
 
 // Rolling dice function
 function rollingDice() {
   // Generate random number
+  if (!playing) return;
   const randomNumber = Math.trunc(Math.random() * 6 + 1);
-  console.log(randomNumber);
 
   diceEl.src = `dice-${randomNumber}.png`;
   if (diceEl.classList.contains("hidden")) diceEl.classList.remove("hidden");
 
   // Check if the number is 1
   if (randomNumber === 1) {
-    // Check which player is playing now then make his score to 0 and switch the players
-    if (
-      document.querySelector(".player--0").classList.contains("player--active")
-    ) {
-      player1CurrentScoreEl.textContent = 0;
-      document.querySelector(".player--0").classList.remove("player--active");
-      document.querySelector(".player--1").classList.add("player--active");
-    } else {
-      player2CurrentScoreEl.textContent = 0;
-      document.querySelector(".player--1").classList.remove("player--active");
-      document.querySelector(".player--0").classList.add("player--active");
-    }
+    switchPlayers();
   } else {
     // Check which player is playing now
     if (
@@ -52,8 +41,62 @@ function rollingDice() {
     )
       player1CurrentScoreEl.textContent =
         Number(player1CurrentScoreEl.textContent) + randomNumber;
-    else
+    else if (
+      document.querySelector(".player--1").classList.contains("player--active")
+    )
       player2CurrentScoreEl.textContent =
         Number(player2CurrentScoreEl.textContent) + randomNumber;
+    else diceEl.classList.add("hidden");
+  }
+}
+
+// Holding score function
+function holdScore() {
+  if (!playing) return;
+
+  if (
+    document.querySelector(".player--0").classList.contains("player--active")
+  ) {
+    player1ScoreEl.textContent =
+      Number(player1ScoreEl.textContent) +
+      Number(player1CurrentScoreEl.textContent);
+
+    if (Number(player1ScoreEl.textContent) >= 15) {
+      playing = false;
+      document.querySelector(".player--0").classList.add("player--winner");
+      document.querySelector(".player--0").classList.remove("player--active");
+      diceEl.classList.add("hidden");
+    } else switchPlayers();
+  } else if (
+    document.querySelector(".player--1").classList.contains("player--active")
+  ) {
+    player2ScoreEl.textContent =
+      Number(player2ScoreEl.textContent) +
+      Number(player2CurrentScoreEl.textContent);
+
+    if (Number(player2ScoreEl.textContent) >= 15) {
+      playing = false;
+      document.querySelector(".player--1").classList.add("player--winner");
+      document.querySelector(".player--1").classList.remove("player--active");
+      diceEl.classList.add("hidden");
+    } else switchPlayers();
+  }
+}
+
+// Helper functions
+function switchPlayers() {
+  // Check which player is playing now then make his score to 0 and switch the players
+
+  if (!playing) return;
+  if (
+    document.querySelector(".player--0").classList.contains("player--active")
+  ) {
+    player1CurrentScoreEl.textContent = 0;
+    document.querySelector(".player--0").classList.remove("player--active");
+    document.querySelector(".player--1").classList.add("player--active");
+  } else {
+    player2CurrentScoreEl.textContent = 0;
+    document.querySelector(".player--1").classList.remove("player--active");
+    document.querySelector(".player--0").classList.add("player--active");
   }
 }
